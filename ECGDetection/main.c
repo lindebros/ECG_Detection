4f30136a1c3a9a4bf96edf2bee9e269bd2b12de2
Mathias Linde <math.linde1984@gmail.com>
2015-09-27 19:35:38 +0200

    Looking for solving last issues.

--- ECGDetection/main.c
+++ ECGDetection/main.c
@@ -2,67 +2,67 @@
 #include"sensor.h"
 #include<time.h>
 
-void findPeaks(long long input);
+void findPeaks(int input);
 
-void findRPeaks(long long peak);
-void notRPeak(long long peak);
-void correctInterval(long long peak);
+void findRPeaks(int peak);
+void notRPeak(int peak);
+void correctInterval(int peak);
 void moveRecentRROk();
 void moveRecentRR();
 void calculateAverage();
-void searchBack(long long peak2);
+void searchBack(int peak2);
 
-void dataOutput(long long peak, long long RR, long long place);
+void dataOutput(int peak, int RR, int place);
 
-long long index = 0;
+long index = 0;
 
-long long NPKF = 0;
-long long SPKF = 0;
-long long THRESHOLD1 = 0;
-long long THRESHOLD2 = 0;
+int NPKF = 0;
+int SPKF = 0;
+int THRESHOLD1 = 0;
+int THRESHOLD2 = 0;
 
-long long RR = 0;
-long long olderIndex = 0;
+int RR = 0;
+int olderIndex = 0;
 
-long long RR_LOW = 0;
-long long RR_HIGH = 0;
-long long RR_MISS = 0;
+int RR_LOW = 0;
+int RR_HIGH = 0;
+int RR_MISS = 0;
 
-long long RPEAKS[2000] = {0};
-long long currentRIndex = 0;
+int RPEAKS[2000] = {0};
+int currentRIndex = 0;
 
-long long RECENTRR_OK[8] = {0};
-long long RECENTRR[8] = {0};
+int RECENTRR_OK[8] = {0};
+int RECENTRR[8] = {0};
 
-long long RR_AVERAGE1;
-long long RR_AVERAGE2;
+int RR_AVERAGE1;
+int RR_AVERAGE2;
 
-long long RR_ERROR = 0;
+int RR_ERROR = 0;
 
-long long main()
+int main()
 {
 	clock_t  start, end;
 	double cpu_time_used;
 
-	static const char filename[] = "ECG900K.txt";
+	static const char filename[] = "ECG.txt";
 
 	FILE *file = fopen(filename,"r");
 
-	long long incoming;
-	long long lowPassOut;
-	long long highPassOut;
-	long long deriveOut;
-	long long squareOut;
-	long long intSum;
-	long long mWIOut;
+	int incoming;
+	int lowPassOut;
+	int highPassOut;
+	int deriveOut;
+	int squareOut;
+	int intSum;
+	int mWIOut;
 
-	long long oldRaw[12] = {0};
-	long long oldLowOut[32] = {0};
-	long long oldHighOut[4] = {0};
-	long long oldSquareOut[30] = {0};
+	int oldRaw[12] = {0};
+	int oldLowOut[32] = {0};
+	int oldHighOut[4] = {0};
+	int oldSquareOut[30] = {0};
 
-	long long OSO_index = 0 ;
-	long long raw_index = 0 ;
+	int OSO_index = 0 ;
+	int raw_index = 0 ;
 
 
 	incoming = getNextData(file);
@@ -82,30 +82,29 @@ long long main()
 		squareOut = deriveOut * deriveOut;
 
 		//Moving Window Integrator
-		oldSquareOut[OSO_index] = squareOut;
-		if(OSO_index == 29){
-			OSO_index = 0;
-		}else{
-			OSO_index++;
+
+		for(int i = 29 ; i >= 1 ; i--){
+			oldSquareOut[i] = oldSquareOut[i - 1];
 		}
+		oldSquareOut[0] = squareOut;
 
 		intSum = 0;
-		for(long long i = 29 ; i >= 1; i--){
+		for(int i = 29 ; i >= 1; i--){
 			intSum += oldSquareOut[i] / 30;
 		}
 		mWIOut = intSum;
 
 		//Updating the lists.
-		for(long long i = 11 ; i >= 1 ; i--){
+		for(int i = 11 ; i >= 1 ; i--){
 			oldRaw[i] = oldRaw[i-1];
 		}
 
-		for(long long i = 31 ; i >= 1 ; i--){
+		for(int i = 31 ; i >= 1 ; i--){
 			oldLowOut[i] = oldLowOut[i-1];
 		}
 
 
-		for(long long i = 3 ; i >= 1 ; i--){
+		for(int i = 3 ; i >= 1 ; i--){
 			oldHighOut[i] = oldHighOut[i-1];
 		}
 
@@ -135,26 +134,26 @@ long long main()
 	return 0;
 }
 
-void findPeaks(long long input){
+void findPeaks(int input){
 
-	static long long peakCheck[9] = {0};
+	static int peakCheck[9] = {0};
 
-	long long biggestLeft = 0;
-	long long biggestRight = 0;
+	int biggestLeft = 0;
+	int biggestRight = 0;
 
-	for(long long i = 8 ; i >= 1 ; i--){
+	for(int i = 8 ; i >= 1 ; i--){
 		peakCheck[i] = peakCheck[i - 1];
 	}
 	peakCheck[0] = input;
 
 
-	for(long long i = 0 ; i < 4 ; i++){
+	for(int i = 0 ; i < 4 ; i++){
 		if(peakCheck[i] > biggestLeft){
 			biggestLeft = peakCheck[i];
 		}
 	}
 
-	for(long long i = 5 ; i < 9 ; i++){
+	for(int i = 5 ; i < 9 ; i++){
 		if(peakCheck[i] > biggestRight){
 			biggestRight = peakCheck[i];
 		}
@@ -168,10 +167,10 @@ void findPeaks(long long input){
 
 }
 
-void findRPeaks(long long peak){
-	static long long PEAKS[2000] = {0};
-	static long long INDEXES[2000] = {0};
-	static long long PEAKINDEX = 0;
+void findRPeaks(int peak){
+	static int PEAKS[2000] = {0};
+	static int INDEXES[2000] = {0};
+	static int PEAKINDEX = 0;
 
 
 	PEAKS[PEAKINDEX] = peak;
@@ -183,7 +182,9 @@ void findRPeaks(long long peak){
 	}else{
 		RR = (index - 4) - olderIndex;
 
-		if(RR < RR_HIGH && RR > RR_LOW){
+		if(RR < RR_HIGH){
+			(RR > RR_LOW){
+		}
 				correctInterval(peak);
 
 				olderIndex = index - 4;
@@ -205,12 +206,12 @@ void findRPeaks(long long peak){
 
 		if(RR > RR_MISS){
 
-			long long tempIndex = PEAKINDEX -2;
+			int tempIndex = PEAKINDEX -2;
 
 			while(tempIndex >= 0 ){
 				if(*(PEAKS + tempIndex) > THRESHOLD2){
-					long long tempPeak = PEAKS[tempIndex];
-					long long tempPeakIndex = INDEXES[tempIndex];
+					int tempPeak = PEAKS[tempIndex];
+					int tempPeakIndex = INDEXES[tempIndex];
 					tempIndex = -1;
 
 					RR = (index - 4) - tempPeakIndex;
@@ -234,7 +235,7 @@ void findRPeaks(long long peak){
 
 }
 
-void notRPeak(long long peak){
+void notRPeak(int peak){
 
 	NPKF = (peak / 8) + (NPKF * 7 / 8);
 	THRESHOLD1 = NPKF + ((SPKF - NPKF) / 4);
@@ -245,7 +246,7 @@ void notRPeak(long long peak){
 	}
 }
 
-void correctInterval(long long peak){
+void correctInterval(int peak){
 	*(RPEAKS + currentRIndex) = peak;
 	currentRIndex++ ;
 
@@ -269,13 +270,13 @@ void correctInterval(long long peak){
 }
 
 void moveRecentRROk(){
-	for(long long i = 7 ; i > 0 ; i--){
+	for(int i = 7 ; i > 0 ; i--){
 		*(RECENTRR_OK + i) = *(RECENTRR_OK + i - 1);
 	}
 }
 
 void moveRecentRR(){
-	for(long long i = 7 ; i > 0 ; i--){
+	for(int i = 7 ; i > 0 ; i--){
 		*(RECENTRR + i) = *(RECENTRR + i - 1);
 	}
 }
@@ -283,7 +284,7 @@ void moveRecentRR(){
 void calculateAverage(){
 	RR_AVERAGE1 = 0;
 	RR_AVERAGE2 = 0;
-	for(long long i = 7 ; i >= 0 ; i--){
+	for(int i = 7 ; i >= 0 ; i--){
 		RR_AVERAGE2 += *(RECENTRR_OK + i);
 		RR_AVERAGE1 += *(RECENTRR + i);
 	}
@@ -292,14 +293,14 @@ void calculateAverage(){
 
 }
 
-void searchBack(long long peak2){
+void searchBack(int peak2){
 	moveRecentRR();
 	*(RECENTRR) = RR;
 
 	SPKF = (peak2 / 4) + (SPKF * 3 / 4);
 
 	RR_AVERAGE1 = 0;
-	for(long long i = 7 ; i >= 0 ; i--){
+	for(int i = 7 ; i >= 0 ; i--){
 		RR_AVERAGE1 += *(RECENTRR + i)/8;
 	}
 	RR_AVERAGE1 = RR_AVERAGE1 / 8;
@@ -313,7 +314,7 @@ void searchBack(long long peak2){
 
 }
 
-void dataOutput(long long peak, long long RR, long long place){
+void dataOutput(int peak, int RR, int place){
 	float time = place / 250.0;
 	float pulse = 15000.0 / RR;
 
